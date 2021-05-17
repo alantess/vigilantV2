4f5bbca6990b96f37d9bfce4d57ca703ab8a008d
Alan <alantessier97@gmail.com>
2021-05-16 20:40:33 -0400

    opencv working

--- app/vigilant/src/main.cpp
+++ app/vigilant/src/main.cpp
@@ -1,23 +1,31 @@
+#include "camera.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <iostream>
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
-    QGuiApplication app(argc, argv);
+  device deviceinfo{"android"};
+  auto img = get_image(&deviceinfo);
+  QImage imdisplay(
+      (uchar *)img.data, img.cols, img.rows, img.step,
+      QImage::Format_RGB888); // Converts the CV image into Qt standard format
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
+  QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+  const QUrl url(QStringLiteral("qrc:/main.qml"));
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+          QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
+  engine.load(url);
 
-    return app.exec();
+  return app.exec();
 }
