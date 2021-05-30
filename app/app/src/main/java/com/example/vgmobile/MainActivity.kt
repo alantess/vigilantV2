c5c74db020eb6a1c61ddf061d477a7dbb89686c1
Alan <alantessier97@gmail.com>
2021-05-30 10:04:27 -0400

    mobile added

---
+++ app/app/src/main/java/com/example/vgmobile/MainActivity.kt
@@ -0,0 +1,11 @@
+pchroackage com.example.vgmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
