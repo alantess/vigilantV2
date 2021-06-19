a91b1e8eabe64c33807dc5c46091d3d12011bcda
Alan <alantessier97@gmail.com>
2021-06-19 10:42:08 -0400

    added some calibration

--- common/ptcloud/generate_cloud.py
+++ common/ptcloud/generate_cloud.py
@@ -3,14 +3,28 @@ import cv2 as cv
 
 VIDEO = "../../etc/videos/driving.mp4"
 
-W, H = 512, 1024
+H, W = 1024, 512
 
 
 class FeatureExtractor(object):
     def __init__(self):
         self.cap = cv.VideoCapture(VIDEO)
-
         self.orb = cv.ORB_create()
+        # Camera intrinsics and extrinsics
+        self.P = self._calibrate()
+
+        # Camera intrinsics and extrinsics
+        self.c_u = self.P[0, 2]
+        self.c_v = self.P[1, 2]
+        self.f_u = self.P[0, 0]
+        self.f_v = self.P[1, 1]
+
+    def _calibrate(self):
+        res = np.eye(3, k=0)
+        res[0, 2] = W // 2
+        res[1, 2] = H // 2
+        print(res)
+        return res
 
     def show(self):
         while self.cap.isOpened():
@@ -18,6 +32,7 @@ class FeatureExtractor(object):
             self._extract_and_display(frame)
             if cv.waitKey(1) == ord('q'):
                 break
+        cv.destroyAllWindows()
 
     def _extract_and_display(self, img):
         img = cv.resize(img, (H, W))
@@ -41,13 +56,37 @@ class FeatureExtractor(object):
                              flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
         # Extracts ORB
         feats = cv.goodFeaturesToTrack(
-            np.linalg.norm(img, axis=2).astype(np.uint8), 1500, 0.01, 3)
+            np.linalg.norm(img, axis=2, ord=2).astype(np.uint8), 2500, 0.01, 3)
         kps = [cv.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in feats]
         kps, des = self.orb.compute(img, kps)
+
+        # pts = cv.KeyPoint_convert(kps)
+
         img = cv.drawKeypoints(img, kps, None, color=(0, 255, 0), flags=0)
+
         cv.imshow('video', img)
+
+    def lidar_generation(self, uv):
+        # Requires nx3 dims
+        # z = uv[0] --> Pixel Count
+        # x = (uv[0] - cu) * uv[2] / f_u
+        # y = (uv[1] - cv) * uv[2]  / fv
+        # returns zx3 array
+        pass
+
+    def _reshape_pts(self, pts):
+        mesh_x, mesh_y = np.meshgrid(pts, pts)
+        z = np.sinc((np.power(mesh_x, 2) + np.power(mesh_y, 2)))
+        z_norm = (z - z.min()) / (z.max() - z.min())
+        xyz = np.zeros((np.size(mesh_x), 3))
+        xyz[:, 0] = np.reshape(mesh_x, -1)
+        xyz[:, 1] = np.reshape(mesh_y, -1)
+        xyz[:, 2] = np.reshape(z_norm, -1)
+        print(xyz.shape)
+        return xyz
 
 
 if __name__ == '__main__':
     feat = FeatureExtractor()
+
     feat.show()
