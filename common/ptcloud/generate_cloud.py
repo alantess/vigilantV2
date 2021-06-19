976340274926e3774acb87b0ad3b941e7f45a02c
Alan <alantessier97@gmail.com>
2021-06-19 19:10:13 -0400

    3d visualizer working

--- common/ptcloud/generate_cloud.py
+++ common/ptcloud/generate_cloud.py
@@ -1,13 +1,17 @@
 import numpy as np
 import cv2 as cv
+import open3d as o3d
+from sklearn.preprocessing import MinMaxScaler
 
 VIDEO = "../../etc/videos/driving.mp4"
 
-H, W = 1024, 512
+W, H = 1024, 512
 
 
 class FeatureExtractor(object):
     def __init__(self):
+
+        self.scaler = MinMaxScaler()
         self.cap = cv.VideoCapture(VIDEO)
         self.orb = cv.ORB_create()
         # Camera intrinsics and extrinsics
@@ -18,24 +22,36 @@ class FeatureExtractor(object):
         self.c_v = self.P[1, 2]
         self.f_u = self.P[0, 0]
         self.f_v = self.P[1, 1]
+        # Gui Settings for Point Cloud
+        self.vis = o3d.visualization.Visualizer()
+        self.vis.create_window(width=W, height=H, top=600, left=650)
+        opt = self.vis.get_render_option()
+        opt.background_color = np.asarray([0, 0, 0])
+        geometry = o3d.geometry.PointCloud()
+        self.vis.add_geometry(geometry)
+
+        # self.vis.run()
 
     def _calibrate(self):
         res = np.eye(3, k=0)
         res[0, 2] = W // 2
         res[1, 2] = H // 2
-        print(res)
         return res
 
     def show(self):
+        print("Press Q to start.")
         while self.cap.isOpened():
             ret, frame = self.cap.read()
             self._extract_and_display(frame)
+
             if cv.waitKey(1) == ord('q'):
                 break
         cv.destroyAllWindows()
+        self.vis.destroy_window()
 
+    # Extracts features
     def _extract_and_display(self, img):
-        img = cv.resize(img, (H, W))
+        img = cv.resize(img, (W, H))
         # Splits images in Half
         left = img[:, :int(H / 2)].astype(np.uint8)
         right = img[:, int(H / 2):].astype(np.uint8)
@@ -56,34 +72,53 @@ class FeatureExtractor(object):
                              flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
         # Extracts ORB
         feats = cv.goodFeaturesToTrack(
-            np.linalg.norm(img, axis=2, ord=2).astype(np.uint8), 2500, 0.01, 3)
+            np.mean(img, axis=2).astype(np.uint8), 3500, 0.01, 3)
         kps = [cv.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in feats]
         kps, des = self.orb.compute(img, kps)
 
-        # pts = cv.KeyPoint_convert(kps)
+        pts = cv.KeyPoint_convert(kps)
+        self.lidar_generation(pts)
 
         img = cv.drawKeypoints(img, kps, None, color=(0, 255, 0), flags=0)
 
         cv.imshow('video', img)
 
-    def lidar_generation(self, uv):
+    def lidar_generation(self, pts, max_high=1):
         # Requires nx3 dims
         # z = uv[0] --> Pixel Count
         # x = (uv[0] - cu) * uv[2] / f_u
         # y = (uv[1] - cv) * uv[2]  / fv
-        # returns zx3 array
-        pass
+        uv_depth = self._2d_to_3d(pts)
+        n = uv_depth.shape[0]
+        x = ((uv_depth[:, 0] - self.c_u) * uv_depth[:, 2]) / self.f_u
+        y = ((uv_depth[:, 1] - self.c_v) * uv_depth[:, 2]) / self.f_v
+        pts_3d_rect = np.zeros((n, 3))
+        pts_3d_rect[:, 0] = x
+        pts_3d_rect[:, 1] = y
+        pts_3d_rect[:, 2] = uv_depth[:, 2]
+        pts_3d_rect = self.scaler.fit_transform(pts_3d_rect)
 
-    def _reshape_pts(self, pts):
-        mesh_x, mesh_y = np.meshgrid(pts, pts)
-        z = np.sinc((np.power(mesh_x, 2) + np.power(mesh_y, 2)))
-        z_norm = (z - z.min()) / (z.max() - z.min())
-        xyz = np.zeros((np.size(mesh_x), 3))
-        xyz[:, 0] = np.reshape(mesh_x, -1)
-        xyz[:, 1] = np.reshape(mesh_y, -1)
-        xyz[:, 2] = np.reshape(z_norm, -1)
-        print(xyz.shape)
-        return xyz
+        valid = (pts_3d_rect[:, 0] >= 0) & (pts_3d_rect[:, 2] < 1)
+        pts_3d_rect = pts_3d_rect[valid]
+        self.display_lidar(pts_3d_rect)
+
+    def display_lidar(self, xyz):
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(xyz)
+        ctr = self.vis.get_view_control()
+        ctr.rotate(10, 0.0, 0.0)
+
+        self.vis.clear_geometries()
+        self.vis.add_geometry(pcd)
+        self.vis.run()
+
+    def _2d_to_3d(self, pts):
+        rows, cols = pts.shape
+        c, r = np.meshgrid(np.arange(cols), np.arange(rows))
+        points = np.stack([c, r, pts])
+        points = points.reshape((3, -1))
+        points = points.T
+        return points
 
 
 if __name__ == '__main__':
